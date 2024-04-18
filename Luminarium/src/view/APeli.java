f678e5413ec892ca1d82119f5cd30171ed193e4f
Aitziber <eskizabel.aitziber@gmail.com>
2024-04-18 14:16:25 +0200

    ventana MenuAdmin lanza todas las ventanas diseñadas para admin. ventanas A,B,E,V reciben controlador y user e implementan ActionListener.

--- Luminarium/src/view/APeli.java
+++ Luminarium/src/view/APeli.java
@@ -1,23 +1,45 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class APeli extends JFrame {
+import controller.Controller;
+import model.Usuario;
+
+import javax.swing.JLabel;
+
+public class APeli extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private Controller controlador;
+	private Usuario user;
 
-	public APeli() {
+	public APeli(Controller c, Usuario u) {
+		this.controlador=c;
+		this.user=u;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("APeli");
+		lblNewLabel.setBounds(91, 100, 45, 13);
+		contentPane.add(lblNewLabel);
+		
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- Luminarium/src/view/VPeli.java
+++ Luminarium/src/view/VPeli.java
@@ -11,13 +11,19 @@ import model.Usuario;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class VPeli extends JFrame {
+public class VPeli extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private Controller controlador;
+	private Usuario user;
 
-	public VPeli(Usuario user, Controller c) {
+	public VPeli(Controller c, Usuario u) {
+		this.controlador=c;
+		this.user=u;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -31,6 +37,12 @@ public class VPeli extends JFrame {
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(171, 35, 124, 39);
 		contentPane.add(lblNewLabel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
