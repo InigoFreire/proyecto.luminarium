6019622352f99dc26b16edfffab203402310fecb
Keli112 <kbilbao22@gmail.com>
2024-05-10 09:06:10 +0200

    testeado testes y corregido PeliculaTest.java

--- Luminarium/src/TestUnitarios/PeliculaTest.java
+++ Luminarium/src/TestUnitarios/PeliculaTest.java
@@ -16,11 +16,11 @@ public class PeliculaTest {
      */
     @Test
     void testConstructorAndGetters() {
-        Genero genero = Genero.valueOf("Accion");
+        Genero genero = Genero.valueOf("Accion".toUpperCase());
         Pelicula pelicula = new Pelicula("1", genero, "Titanic", 12, 180, "Una gran historia de amor");
 
         assertEquals("1", pelicula.getId());
-        assertEquals(genero, pelicula.getGenero());
+        assertEquals("Accion".toUpperCase(), pelicula.getGenero().toString());
         assertEquals("Titanic", pelicula.getTitulo());
         assertEquals(12, pelicula.getPegi());
         assertEquals(180, pelicula.getDuracion());
@@ -34,18 +34,19 @@ public class PeliculaTest {
     void testSetters() {
         Pelicula pelicula = new Pelicula();
         pelicula.setId("2");
-        pelicula.setGenero(Genero.valueOf("Comedia"));
+        pelicula.setGenero(Genero.valueOf("Comedia".toUpperCase()));
         pelicula.setTitulo("Superbad");
         pelicula.setPegi(18);
         pelicula.setDuracion(120);
         pelicula.setSinopsis("Dos amigos viven una noche épica antes de ir a la universidad");
 
         assertEquals("2", pelicula.getId());
-        assertEquals("Comedia", pelicula.getGenero());
+        assertEquals("Comedia".toUpperCase(), pelicula.getGenero().toString());
         assertEquals("Superbad", pelicula.getTitulo());
         assertEquals(18, pelicula.getPegi());
         assertEquals(120, pelicula.getDuracion());
         assertEquals("Dos amigos viven una noche épica antes de ir a la universidad", pelicula.getSinopsis());
     }
 }
+
 
